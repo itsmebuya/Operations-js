0d30380026f8ab73b59114ae33e3cc36b12fc15e
24LP7490 24LP7490 <itsmebuya@gmail.com>
2024-12-02 18:12:32 +0800

    ope

---
+++ index.js
@@ -0,0 +1,59 @@
+// 8
+const a8 = 13;
+const b8 = 12;
+const c8 = 11;
+const d8 = a8 + b8 + c8
+console.log("Gurwaljingin talbai n " + d8);
+
+// 9 
+const radius = 10;
+const result9 = Math.PI * 2 * radius
+console.log("toirgin talbai " + result9);
+
+//10
+const y1 = 2;
+const y2 = 5;
+const x1 = 3;
+const x2 = 4;
+const result10 = (y2-y1)/(x2-x1)
+console.log("m tentsuu " + result10);
+
+//11
+const data = 15;
+const rate = 3;
+const result11 = data/rate;
+console.log("bit rate n " + result11);
+
+//12
+const side1 = 10;
+const side2 = 20;
+const side3 = 15;
+const result12 = (side1 * side2 * side3 )/2
+console.log(result12);
+
+//13
+let cel = 15;
+let faren = cel * 9 / 5 +32
+console.log("farenheit " +faren);
+
+//14 
+let teama = (96+ 108 + 89) /3
+let teamb = (88 +91 +110) /3
+console.log("a bagin dundaj onoo " + teama + "b bagin dundaj onoo " + teamb);
+
+//16
+
+let cm = 10;
+let inch = cm * 0.3937
+console.log("10cm n incher bol " +inch);
+
+//17
+let too = prompt("gurwaljingin radiusiig oruulna uu");
+let hariu = too*2*Math.PI
+console.log("gurwaljingin ezelhun n " +hariu)
+
+//18
+let a = 123;
+let b = a/100;
+let c = a%10;
+console.log("a toonii ehnii tsifr n " + b + c);
